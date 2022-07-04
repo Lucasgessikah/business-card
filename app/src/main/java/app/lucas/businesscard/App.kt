fcca355164a8e7ba412a27c1be92e1ce361d6acd
Lucasgessikah <104868677+Lucasgessikah@users.noreply.github.com>
2022-07-04 20:26:51 -0300

    Criando repositório

---
+++ app/src/main/java/app/lucas/businesscard/App.kt
@@ -0,0 +1,6 @@
+package app.lucas.businesscard
+
+import android.app.Application
+
+class App: Application() {
+}
---
+++ app/src/main/java/app/lucas/businesscard/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package app.lucas.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.lucas.businesscard.R
+import app.lucas.businesscard.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
