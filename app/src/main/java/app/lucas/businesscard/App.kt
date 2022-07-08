f937b47a8da21af214061bf50de1fcee0f1b7749
Lucasgessikah <104868677+Lucasgessikah@users.noreply.github.com>
2022-07-07 22:23:13 -0300

    Atualização com possíveis erros

--- app/src/main/java/app/lucas/businesscard/App.kt
+++ app/src/main/java/app/lucas/businesscard/App.kt
@@ -1,6 +1,10 @@
 package app.lucas.businesscard
 
 import android.app.Application
+import app.lucas.businesscard.data.AppDatabase
+import app.lucas.businesscard.data.BusinessCardRepository
 
 class App: Application() {
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
